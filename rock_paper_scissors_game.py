0297cd99413d6c6d5aa1b98efb074ae4d3d86040
Crysis760 <alantokaev2004@gmail.com>
2023-08-21 18:07:47 +0200

    Deleted unnecessary items

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -9,19 +9,19 @@ import random
 
 def choice1():
     global player_choice
-    player_choice = "Камень"
+    player_choice = "rock"
     start_game()
 
 
 def choice2():
     global player_choice
-    player_choice = "Ножницы"
+    player_choice = "scissors"
     start_game()
 
 
 def choice3():
     global player_choice
-    player_choice = "Бумагу"
+    player_choice = "paper"
     start_game()
 
 
@@ -40,45 +40,44 @@ def end_game():
     gameWindow.geometry("600x200")
     gameWindow.resizable(width=False, height=False)
     gameWindow.title("Rock Paper Scissors")
-    gameWindow.iconphoto(False, tkinter.PhotoImage(file="C:\\Users\\crysis76\\Desktop\\exe py projects\\"
-                                                        "rock paper scissors\\main_Icon.png"))
+    gameWindow.iconphoto(False, tkinter.PhotoImage(file=".\\media\\main_Icon.png"))
 
     goodbye_label1 = Label(gameWindow,
                            font=("Arial", 20),
-                           text="Игра окончена!")
+                           text="Game over!")
     goodbye_label1.pack(side=TOP)
 
     end_label = Label(gameWindow)
-    if winner == "Ничья":
+    if winner == "Draw":
         end_label.config(font=("Arial", 15),
-                         text="Вы сыграли в ничью, так как компьютер выбрал " + pc_choice +
-                              ", \n вы выбрали " + player_choice)
+                         text="It's draw, because computer chose " + pc_choice +
+                              ", \n you chose " + player_choice)
         end_label.pack()
-    elif winner == "Игрок":
+    elif winner == "Player":
         end_label.config(font=("Arial", 15),
-                         text="Вы победили, так как компьютер выбрал " + pc_choice +
-                              ", \n вы выбрали " + player_choice)
+                         text="You won, because computer chose " + pc_choice +
+                              ", \n you chose " + player_choice)
         end_label.pack()
     else:
         end_label.config(font=("Arial", 15),
-                         text="Вы проиграли, так как компьютер выбрал " + pc_choice +
-                              ", \n вы выбрали " + player_choice)
+                         text="You lost, because computer chose " + pc_choice +
+                              ", \n you chose " + player_choice)
         end_label.pack()
 
     goodbye_label2 = Label(gameWindow,
                            font=("Arial", 20),
-                           text="Хотите сыграть ещё?")
+                           text="Wanna try again?")
     goodbye_label2.pack()
 
     repeat_button = Button(gameWindow,
                            font=("Arial", 20),
-                           text="Заново",
+                           text="Again",
                            command=repeat_game)
     repeat_button.place(x=100, y=130)
 
     exit_button = Button(gameWindow,
                          font=("Arial", 20),
-                         text="Выйти",
+                         text="Quit game",
                          command=exit_game)
     exit_button.place(x=400, y=130)
 
@@ -86,25 +85,24 @@ def end_game():
 def start_game():
     global winner
     global pc_choice
-    pc_choice = random.choice(["Камень", "Ножницы", "Бумагу"])
-    if player_choice == "Камень" and pc_choice == "Камень":
-        winner = "Ничья"
-    elif player_choice == "Камень" and pc_choice == "Ножницы":
-        winner = "Игрок"
-    elif player_choice == "Камень" and pc_choice == "Бумагу":
-        winner = "Компьютер"
-    elif player_choice == "Ножницы" and pc_choice == "Ножницы":
-        winner = "Ничья"
-    elif player_choice == "Ножницы" and pc_choice == "Бумагу":
-        winner = "Игрок"
-    elif player_choice == "Ножницы" and pc_choice == "Камень":
-        winner = "Компьютер"
-    elif player_choice == "Бумагу" and pc_choice == "Бумагу":
-        winner = "Ничья"
-    elif player_choice == "Бумагу" and pc_choice == "Камень":
-        winner = "Игрок"
-    else:
-        winner = "Компьютер"
+    pc_choice = random.choice(["rock", "scissors", "paper"])
+    if player_choice == pc_choice:
+        winner = "Draw"
+    elif player_choice == "rock":
+        if pc_choice == "scissors":
+            winner = "Player"
+        else:
+            winner = "Computer"
+    elif player_choice == "scissors":
+        if pc_choice == "paper":
+            winner = "Player"
+        else:
+            winner = "Computer"
+    elif player_choice == "paper":
+        if pc_choice == "rock":
+            winner = "Player"
+        else:
+            winner = "Computer"
 
     end_game()
 
@@ -117,32 +115,30 @@ window = Tk()
 window.geometry("600x200")
 window.resizable(width=False, height=False)
 window.title("Rock Paper Scissors")
-window.iconphoto(False, tkinter.PhotoImage(file="C:\\Users\\crysis76\\Desktop\\exe py projects\\"
-                                                "rock paper scissors\\main_Icon.png"))
+window.iconphoto(False, tkinter.PhotoImage(file=".\\media\\main_Icon.png"))
 
 # --------------------------------------------------
 # GAME FRONTEND
 
 HelloLabel = Label(window,
-                   text="Здравствуйте, для начала игры выберите, \n"
-                        "за кого будете играть:",
+                   text="Hello, choose your item",
                    font=("Arial", 20))
 HelloLabel.pack()
 
 Button1 = Button(window,
-                 text="Камень",
+                 text="rock",
                  font=("Arial", 25),
                  command=choice1)
 Button1.place(x=40, y=100)
 
 Button2 = Button(window,
-                 text="Ножницы",
+                 text="scissors",
                  font=("Arial", 25),
                  command=choice2)
 Button2.place(x=215, y=100)
 
 Button3 = Button(window,
-                 text="Бумага",
+                 text="paper",
                  font=("Arial", 25),
                  command=choice3)
 Button3.place(x=420, y=100)
